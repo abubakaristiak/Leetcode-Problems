a23d733fa717f2db89603fdced8e2845f826b2ec
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-10-08 18:43:48 +0600

    75 days coding of day-6

--- Medium/_151_Reverse Words in a String.cpp
+++ Medium/_151_Reverse Words in a String.cpp
@@ -27,3 +27,25 @@ public:
         return s;
     }
 };
+
+
+// another approach (using STL)
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word;
+        vector<string> words;
+        
+        while(ss>>word){
+            words.push_back(word);
+        }
+        reverse(words.begin(), words.end());
+        string result;
+        for(int i=0; i<words.size(); i++){
+            if(i>0) result += ' ';
+            result += words[i];
+        }
+        return result;
+    }
+};
