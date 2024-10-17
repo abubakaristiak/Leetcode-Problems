d582ffa9b706b750efdcb6978e7aead843a70a9c
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-10-17 23:40:52 +0600

    sorting array related problem

--- Medium/_912_Sort an Array.cpp
+++ Medium/_912_Sort an Array.cpp
@@ -1,5 +1,37 @@
 //https://leetcode.com/problems/sort-an-array/
+
+
 /*
+TODO: Counting sort;
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        unordered_map<int,int> mp;
+
+        for(int &num : nums){
+            mp[num]++;
+        }
+        int minE=*min_element(begin(nums), end(nums));
+        int maxE=*max_element(begin(nums), end(nums));
+        
+        int i=0;
+        for(int num=minE; num<=maxE; num++){
+            while(mp[num]>0){
+                nums[i]=num;
+                i++;
+                mp[num]--;
+            }
+        }
+        return nums;
+    }
+};
+
+
+
+
+
+
+TODO: Using built-in function;
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
