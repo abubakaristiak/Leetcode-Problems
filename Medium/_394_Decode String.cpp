6a8ea2ee354fe81c4f26f147f11a4a2f409caa2c
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-10-29 14:48:04 +0600

    75 days coding of day-27

---
+++ Medium/_394_Decode String.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<char> st;
+        for(int i=0; i<s.size(); i++){
+            if(s[i] != ']'){
+                st.push(s[i]);
+            }
+            else{
+                string curStr = "";
+                while(st.top() != '['){
+                    curStr=st.top()+curStr;
+                    st.pop();
+                }
+                st.pop();
+                string num="";
+                while(!st.empty() && isdigit(st.top())){
+                    num=st.top()+num;
+                    st.pop();
+                }
+                int times=stoi(num);
+                while(times--){
+                    for(int j=0; j<curStr.size(); j++){
+                        st.push(curStr[j]);
+                    }
+                }
+            }
+
+        }
+        s="";
+        while(!st.empty()){
+            s=st.top()+s;
+            st.pop();
+        }
+        return s;
+    }
+};
