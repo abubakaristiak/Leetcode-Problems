ea3660c377e0b95ee72474928e1588432aba57ac
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-11-01 18:54:54 +0600

    added new problem named Delete the Middle Node of a Linked List

---
+++ Medium/_2095_Delete the Middle Node of a Linked List.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+// main parts;
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(head==nullptr || head->next==nullptr){
+            return nullptr;
+        }
+        ListNode* prev=nullptr;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            prev=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        prev->next=slow->next;
+        delete(slow);
+        return head;
+        
+    }
+};
