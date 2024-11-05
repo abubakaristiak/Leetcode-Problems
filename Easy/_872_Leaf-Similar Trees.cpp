fc76df74d11aeaf650c169b97a7f034c5e525902
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-11-05 15:19:17 +0600

    added 75 days coding of day-34

--- Easy/_872_Leaf-Similar Trees.cpp
+++ Easy/_872_Leaf-Similar Trees.cpp
@@ -1,16 +1,21 @@
 // TODO: https://leetcode.com/problems/leaf-similar-trees/
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-/*
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+
+// main part;
 class Solution {
 public:
     vector<int>v1;
@@ -37,4 +42,3 @@ public:
     }
 };
 
-*/
