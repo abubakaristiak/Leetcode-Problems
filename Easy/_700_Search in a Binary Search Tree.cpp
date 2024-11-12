5d2109660de6fe9396b8fb5ee9a9359381d226b5
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-11-12 13:52:47 +0600

    added 75 days coding of day-41

--- Easy/_700_Search in a Binary Search Tree.cpp
+++ Easy/_700_Search in a Binary Search Tree.cpp
@@ -1,17 +1,37 @@
-//TODO: https://leetcode.com/problems/search-in-a-binary-search-tree/description/
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Admin    : abubakaristiak
+ * * * * Created : 2024-11-12 || 13:48:32
+ * * * * File    : _700_Search in a Binary Search Tree.cpp
  */
 
-/*
+
+// https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
@@ -22,4 +42,3 @@ public:
         
     }
 };
-*/
