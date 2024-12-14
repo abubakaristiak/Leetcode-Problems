4d7f7be20e6806ecd216e946c56c04a40efd4ce8
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-12-14 12:00:00 +0600

    added new problem

--- Easy/_1534_Count Good Triplets.cpp
+++ Easy/_1534_Count Good Triplets.cpp
@@ -32,6 +32,7 @@ public:
         int n=arr.size();
         int cnt=0;
 
+        
         for(int i=0; i<=n-3; i++){
             for(int j=i+1; j<=n-2; j++){
                 if(abs(arr[i]-arr[j])<=a){
@@ -45,4 +46,4 @@ public:
         }
         return cnt;
     }
-};
+}
