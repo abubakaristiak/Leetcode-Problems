48edebe15b5a245390f24cfe4fd9e3e69eb5c3a9
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-02-25 00:22:25 +0600

    added new problem on leetcode

---
+++ Easy/_258_Add Digits.cpp
@@ -0,0 +1,48 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-02-25 || 00:19:22
+ * * * * File    : _258_Add Digits.cpp
+*/
+
+
+// https://leetcode.com/problems/add-digits/description/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+class Solution {
+public:
+    int addDigits(int num) {
+        ll sum=0;;
+        while (num)
+        {
+            sum+=(num%10);
+            num/=10;
+        }
+        if(sum<10){
+            return sum;
+        }else{
+            return addDigits(sum);
+        }
+        
+    }
+};
