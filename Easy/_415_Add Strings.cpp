4bf838c98e0cb410199a54290e39ba18d3d29a8e
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-02-26 00:14:50 +0600

    added new problems on Leetcode

---
+++ Easy/_415_Add Strings.cpp
@@ -0,0 +1,38 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-02-26 || 00:08:18
+ * * * * File    : _415_Add Strings.cpp
+*/
+
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int val1=stoi(num1);
+        int val2=stoi(num2);
+        int res = val1+val2
+        cout << to_string(res) << endl;
+    }
+};
