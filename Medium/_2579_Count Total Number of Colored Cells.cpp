128e1c4ed4867edc271b0073be2cc4a784a53820
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-03-06 00:46:35 +0600

    added daily problem on Leetcode march-05

---
+++ Medium/_2579_Count Total Number of Colored Cells.cpp
@@ -0,0 +1,42 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-03-06 || 00:44:02
+ * * * * File    : _2579_Count Total Number of Colored Cells.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+class Solution {
+public:
+    long long coloredCells(int n) {
+        int t=1;
+        ll cells=1;
+        while (t<=n)
+        {
+            cells+=(4*(t-1));
+            t++;
+        }
+        return cells;
+        
+    }
+};
