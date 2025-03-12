155cdfd562745c1ac28bb2148a12ff8f69bb151d
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-03-12 23:03:17 +0600

    added new problem

---
+++ Easy/_217_Contains Duplicate.cpp
@@ -0,0 +1,43 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-03-12 || 23:02:41
+ * * * * File    : _217_Contains Duplicate.cpp
+*/
+
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        set<int> st;
+        for(int i=0; i<n; i++){
+            st.insert(nums[i]);
+        }
+        if(st.size()!=n){
+            return true;
+        }
+        return false;
+    }
+};
