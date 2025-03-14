502bfe70685af3f9ca9e111e0f36fd30f370af26
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-03-14 23:10:51 +0600

    added new problem

---
+++ Easy/_145_Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,54 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-03-14 || 23:09:40
+ * * * * File    : _145_Binary Tree Postorder Traversal.cpp
+*/
+
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    public:
+        void postOrder(TreeNode* root, vector<int> &res){
+            if(root==NULL){
+                return;
+            }
+            postOrder(root->left, res);
+            postOrder(root->right, res);
+            res.push_back(root->val);
+        }
+        vector<int> postorderTraversal(TreeNode* root) {
+            vector<int> res;
+            postOrder(root, res);
+            return res;
+        }
+    };
