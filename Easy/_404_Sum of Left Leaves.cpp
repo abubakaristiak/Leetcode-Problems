48949c4faf548f02069d29ce287ff8fda3d2f101
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-03-15 23:32:46 +0600

    updated this problem

--- Easy/_404_Sum of Left Leaves.cpp
+++ Easy/_404_Sum of Left Leaves.cpp
@@ -2,34 +2,6 @@
  * Bismillahir Rahmanir Raheem
  *
  * * * * Coder   : abubakaristiak
- * * * * Created : 2025-03-15 || 23:28:39
- * * * * File    : _404_Sum of Left Leaves.cpp
-*/
-
-
-#include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
-#define ll long long
-#define pi pair<ll, ll>
-#define asort(v) sort(v.begin(), v.end())
-#define rsort(v) sort(v.begin(), v.end(), greater<>())
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-#define cyes cout << "Yes\n"
-#define cno cout << "No\n"
-#define endl "\n"
-#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
-using namespace __gnu_pbds;
-using namespace std;
-
-template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
-
-/*
- * Bismillahir Rahmanir Raheem
- *
- * * * * Coder   : abubakaristiak
  * * * * Created : 2025-03-15 || 14:15:50
  * * * * File    : 404. Sum of Left Leaves.cpp
 */
