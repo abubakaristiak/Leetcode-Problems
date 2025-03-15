39df67e0f08334dface07f3cc9b4ed33df956bf5
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-03-15 23:30:05 +0600

    added new Problem

---
+++ Easy/_404_Sum of Left Leaves.cpp
@@ -0,0 +1,87 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-03-15 || 23:28:39
+ * * * * File    : _404_Sum of Left Leaves.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-03-15 || 14:15:50
+ * * * * File    : 404. Sum of Left Leaves.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int sum=0;
+        while (!q.empty())
+        {
+            int sz=q.size();
+            for(int i=1; i<=sz; i++){
+                TreeNode* f=q.front();
+                q.pop();
+                if(f->left && f->left->left==NULL && f->left->right==NULL){
+                    sum+=f->left->val;
+                }
+                if(f->left!=NULL){
+                    q.push(f->left);
+                }
+                if(f->right!=NULL){
+                    q.push(f->right);
+                }
+            }
+        }
+        return sum;
+        
+    }
+};
