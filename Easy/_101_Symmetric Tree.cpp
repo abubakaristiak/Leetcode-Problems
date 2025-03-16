ad8f9afd49fbdc4dd092288c25351708d8605cb3
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-03-16 20:06:46 +0600

    added new problem

---
+++ Easy/_101_Symmetric Tree.cpp
@@ -0,0 +1,57 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-03-16 || 19:58:11
+ * * * * File    : _101_Symmetric Tree.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool solve(TreeNode* l, TreeNode* r){
+        if(l==NULL && r==NULL){
+            return true;
+        }
+        if(l==NULL || r==NULL){
+            return false;
+        }
+        if(l->val==r->val && solve(l->left, r->right) && solve(l->right, r->left)){
+            return true;
+        }
+        return false;
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(root==NULL){
+            return true;
+        }
+        return solve(root->left, root->right);
+    }
+};
