92c260748d6d08b5f4c1883bf0ceea754d656d3f
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-03-31 00:20:24 +0600

    added new problem

---
+++ Easy/_507_Perfect Number.cpp
@@ -0,0 +1,45 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-03-31 || 00:16:42
+ * * * * File    : _507_Perfect Number.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if(num<=1){
+            return false;
+        }
+        ll sum=0;
+        for(int i=1; i<=num/2; i++){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+        if(num==sum){
+            return true;
+        }else return false;
+    }
+};
