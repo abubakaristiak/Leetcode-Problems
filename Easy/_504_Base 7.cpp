022b66d73f423d72c6ac694a87e9ff64b5f95ee2
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-03-31 22:06:46 +0600

    added new problem

---
+++ Easy/_504_Base 7.cpp
@@ -0,0 +1,46 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-03-31 || 21:59:53
+ * * * * File    : _504_Base 7.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+class Solution {
+public:
+    string convertToBase7(int num) {
+        if(num==0){
+            return "0";
+        }
+        string ans;
+        bool negative=num<0;
+        num=abs(num);
+        while (num>0)
+        {
+            ans=to_string(num%7)+ans;
+            num/=7;
+        }
+        return negative?"-"+ans:ans;
+        
+    }
+};
