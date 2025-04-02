333c582a737836b8fc4d5a4c854fd01260b1267c
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-04-02 22:03:42 +0600

    added new problem

---
+++ Easy/_2873_Maximum Value of an Ordered Triplet I.cpp
@@ -0,0 +1,45 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-04-02 || 21:20:14
+ * * * * File    : _2873_Maximum Value of an Ordered Triplet I.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n = nums.size();
+        ll res=0;
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                for(int k=j+1; k<n; k++){
+                    res = max(res, (ll)(nums[i]-nums[j])*nums[k]);
+                }
+            }
+        }
+        return res;
+        
+    }
+};
