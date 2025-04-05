38a43cf36131f4436e1e65b145d194124d446711
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-04-05 23:39:02 +0600

    added new problem

---
+++ Easy/_1863_Sum of All Subset XOR Totals.cpp
@@ -0,0 +1,34 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-04-05 || 23:28:14
+ * * * * File    : _1863_Sum of All Subset XOR Totals.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        
+    }
+};
