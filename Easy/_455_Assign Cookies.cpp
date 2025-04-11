03dcd8df22e006a4717ca56efa35964e14f73cc4
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-04-11 16:37:43 +0600

    added new problem

---
+++ Easy/_455_Assign Cookies.cpp
@@ -0,0 +1,48 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-04-11 || 16:33:06
+ * * * * File    : _455_Assign Cookies.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int m=g.size();
+        int n=s.size();
+        int i=0, j=0;
+        while (i<m && j<n)
+        {
+            if(g[i]<=s[j]){
+                i++;
+            }
+            j++;
+        }
+        return i;
+        
+    }
+};
