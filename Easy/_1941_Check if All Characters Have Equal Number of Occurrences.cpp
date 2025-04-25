1aa284c93d1ec861f92aa8175ae23a29a2cef9a4
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-04-25 12:35:36 +0600

    added new problem

---
+++ Easy/_1941_Check if All Characters Have Equal Number of Occurrences.cpp
@@ -0,0 +1,44 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-04-25 || 12:18:12
+ * * * * File    : _1941_Check if All Characters Have Equal Number of Occurrences.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        map<char, int> mp;
+        for(auto ch:s){
+            mp[ch]++;
+        }
+        int fr=mp[s[0]];
+        for(auto it:mp){
+            if(it.second != fr){
+                return false;
+            }
+        }
+        return true;
+    }
+};
