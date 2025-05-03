d783ce6ae2b1f05cedfd05dc462ce3c38ec67e02
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-05-04 00:32:11 +0600

    updated this problem

--- Easy/_744_Find Smallest Letter Greater Than Target.cpp
+++ Easy/_744_Find Smallest Letter Greater Than Target.cpp
@@ -39,3 +39,27 @@ public:
         return letters[0];
     }
 };
+
+
+// Binary Search:
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int n=letters.size();
+        int l=0, r=n-1;
+        int pos=-1;
+        while (l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(letters[mid]>target){
+                pos=mid;
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        if(pos==-1){
+            return letters[0];
+        }else letters[pos];
+    }
+};
