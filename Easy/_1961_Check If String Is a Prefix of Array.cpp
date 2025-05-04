85d5f3d40a872f56372390e131a964bd08840a10
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-05-05 00:21:00 +0600

    added new problem

---
+++ Easy/_1961_Check If String Is a Prefix of Array.cpp
@@ -0,0 +1,41 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-05-05 || 00:18:01
+ * * * * File    : _1961_Check If String Is a Prefix of Array.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+class Solution {
+public:
+    bool isPrefixString(string s, vector<string>& words) {
+        string res;
+        for(string word:words){
+            res+=word;
+            if(s==res){
+                return true;
+            }
+        }
+        return false;
+    }
+};
