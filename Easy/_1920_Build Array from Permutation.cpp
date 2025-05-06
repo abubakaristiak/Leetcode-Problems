abb8f6a7556fcc44ef9e76712680353766fd6587
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-05-06 22:53:42 +0600

    added daily problem on Leetcode may-06

---
+++ Easy/_1920_Build Array from Permutation.cpp
@@ -0,0 +1,50 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-05-06 || 22:48:37
+ * * * * File    : _1920_Build Array from Permutation.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ull unsigned long long
+#define pi pair<ll, ll>
+#define vi vector<ll>
+#define vpi vector<pi>
+#define pb push_back
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define sz(x) (ll)(x).size()
+#define afor(i,a,b) for (ll i = (a); i < (b); ++i)
+#define rfor(i,a,b) for (ll i = (a); i >= (b); --i)
+#define asort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).begin(), (v).end(), greater<>())
+#define fast() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n=sz(nums);
+        afor(i,0,n){
+            nums[i]=nums[i]+n*(nums[nums[i]]%n);
+        }
+        afor(i,0,n){
+            nums[i]=nums[i]/n;
+        }
+        return nums;
+    }
+};
