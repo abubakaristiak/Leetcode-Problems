707ab78d70b3e6ac3e5e1ab6a2e6c754d6aa0657
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-05-23 20:57:11 +0600

    added new problem

---
+++ Easy/_2900_Longest Unequal Adjacent Groups Subsequence I.cpp
@@ -0,0 +1,67 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-05-15 || 10:21:03
+ * * * * File    : _2900_Longest Unequal Adjacent Groups Subsequence I.cpp
+*//*
+||-----------------------------------||
+||        Abu Bakar Istiak           ||
+||     Chattogram Polytechnic        ||
+||        Department of CST          ||
+||    abubakar119147@gmail.com       ||
+||-----------------------------------||
+*/
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ull unsigned long long
+#define pi pair<ll, ll>
+#define vi vector<ll>
+#define vpi vector<pi>
+#define pb push_back
+#define endl "\n"
+#define sp \" \"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define sz(x) (ll)(x).size()
+#define minus cout << -1 << endl
+#define zero cout << 0 << endl
+#define MAX 100000
+#define MOD 998244353
+#define afor(i,a,b) for (ll i = (a); i < (b); ++i)
+#define rfor(i,a,b) for (ll i = (a); i >= (b); --i)
+#define asort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).begin(), (v).end(), greater<>())
+#define fast() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        vector<string> res;
+        int n=words.size();
+        if(n==0){
+            return res;
+        }
+        res.push_back(words[0]);
+        int tmp=groups[0];
+        for(int i=1; i<n; i++){
+            if(groups[i]!=tmp){
+                res.push_back(words[i]);
+                tmp=groups[i];
+            }
+        }
+        return res;
+    }
+};
