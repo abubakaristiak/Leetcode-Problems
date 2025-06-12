7749a26cdcbbadd73207ca3c33b85f2ac937ba6e
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-06-12 11:36:30 +0600

    added new problem

---
+++ Medium/_790_Domino and Tromino Tiling790. Domino and Tromino Tiling.cpp
@@ -0,0 +1,67 @@
+/*
+||-----------------------------------||
+||        Abu Bakar Istiak           ||
+||     Chattogram Polytechnic        ||
+||        Department of CST          ||
+||    abubakar119147@gmail.com       ||
+||-----------------------------------||
+*/
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ull unsigned long long
+#define pi pair<ll, ll>
+#define vi vector<ll>
+#define vpi vector<pi>
+#define pb push_back
+#define endl "\n"
+#define sp " "
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define sz(x) (ll)(x).size()
+#define minus cout << -1 << endl
+#define zero cout << 0 << endl
+#define MAX 100000
+#define MOD 998244353
+#define afor(i,a,b) for (ll i = (a); i < (b); ++i)
+#define rfor(i,a,b) for (ll i = (a); i >= (b); --i)
+#define asort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).begin(), (v).end(), greater<>())
+#define fast() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+class Solution {
+public:
+    int M=1000000007;
+    int t[1001];
+
+    int solve(int n){
+        if(n==1 || n==2){
+            return n;
+        }
+        if(t[n]!=-1){
+            return t[n];
+        }
+        if(n==3){
+            return 5;
+        }
+
+        return t[n]=(2*solve(n-1)%M + solve(n-3)%M)%M;
+
+    }
+    int numTilings(int n) {
+        memset(t, -1, sizeof(t));
+
+        return solve(n);
+    }
+};
