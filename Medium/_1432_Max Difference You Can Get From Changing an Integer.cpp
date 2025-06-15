81ffdec7a97fbc8320bc06fb86853bc1474c1675
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-06-15 16:41:55 +0600

    added daily problem on Leetcode june-15

---
+++ Medium/_1432_Max Difference You Can Get From Changing an Integer.cpp
@@ -0,0 +1,69 @@
+/*
+||-----------------------------------||
+||        Abu Bakar Istiak           ||
+||     Chattogram Polytechnic        ||
+||        Department of CST          ||
+||    abubakar119147@gmail.com       ||
+||-----------------------------------||
+*/
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ull unsigned long long
+#define pi pair<ll, ll>
+#define vi vector<ll>
+#define vpi vector<pi>
+#define pb push_back
+#define endl "\n"
+#define sp " "
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define sz(x) (ll)(x).size()
+#define minus cout << -1 << endl
+#define zero cout << 0 << endl
+#define MAX 100000
+#define MOD 998244353
+#define afor(i,a,b) for (ll i = (a); i < (b); ++i)
+#define rfor(i,a,b) for (ll i = (a); i >= (b); --i)
+#define asort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).begin(), (v).end(), greater<>())
+#define fast() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+class Solution {
+public:
+    int maxDiff(int num) {
+        string s1=to_string(num);
+        string s2=s1;
+
+        int idx=s1.find_first_not_of('9');
+        if(idx!=string::npos){
+            char ch=s1[idx];
+            replace(s1.begin(), s1.end(), ch, '9');
+        }
+
+        for(int i=0; i<s2.length(); i++){
+            char ch=s2[i];
+            if(i==0){
+                if(ch!='1'){
+                    replace(s2.begin(), s2.end(), ch, '1');
+                    break;
+                }
+            }else if(ch!='0' && ch!=s2[0]){
+                replace(s2.begin(), s2.end(), ch, '0');
+                break;
+            }
+        }
+        return stoi(s1)-stoi(s2);
+    }
+};
