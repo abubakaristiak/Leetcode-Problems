df195cd7dcc052844f7df348cd4df8f134ee4ee3
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-06-18 23:43:13 +0600

    added daily problem on Leetcode june-18 (python code)

---
+++ Medium/_2966_Divide Array Into Arrays With Max Difference.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        nums.sort()
+        res=[]
+        n=len(nums)
+        for i in range(0,n,3):
+            if i+2>=n or nums[i+2]-nums[i]>k:
+                return []
+            res.append([nums[i], nums[i+1], nums[i+2]])
+        
+        return res
