3f8b28966ede557c0b3a40072ef76de8fe8efb03
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-07-11 18:26:16 +0600

    added new problem

---
+++ Hard/_41_First Missing Positive.py
@@ -0,0 +1,13 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums=[n for n in nums if n>0]
+        nums.sort()
+
+        tar=1
+        for n in nums:
+            if n==tar:
+                tar+=1
+            elif n>tar:
+                return tar
+            
+        return tar
