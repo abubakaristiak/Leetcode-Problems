795c8dd43d97160a402622c784636a93ccbc4bbe
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-07-25 20:19:02 +0600

    added daily problem on Leetcode july-25

---
+++ Easy/_3487_Maximum Unique Subarray Sum After Deletion.cpp
@@ -0,0 +1,68 @@
+/*
+||-----------------------------------||
+||        Abu Bakar Istiak           ||
+||     Chattogram Polytechnic        ||
+||        Department of CST          ||
+||    abubakar119147@gmail.com       ||
+||-----------------------------------||
+*/
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ull unsigned long long
+#define pi pair<ll, ll>
+#define vi vector<ll>
+#define vpi vector<pi>
+#define pb push_back
+#define sp " "
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define sz(x) (ll)(x).size()
+#define minus cout << -1 << endl
+#define zero cout << 0 << endl
+#define MAX 100000
+#define MOD 998244353
+#define afor(i,a,b) for (ll i = (a); i < (b); ++i)
+#define rfor(i,a,b) for (ll i = (a); i >= (b); --i)
+#define asort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).begin(), (v).end(), greater<>())
+#define fast() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        int n=nums.size();
+        bool neg=true;
+        int mx=INT_MIN;
+        for(int i=0; i<n; i++){
+            if(nums[i]>=0){
+                neg=false;
+            }
+            if(nums[i]>mx){
+                mx=nums[i];
+            }
+        }
+        if(neg){
+            return mx;
+        }
+        unordered_set<int> unique(nums.begin(), nums.end());
+        int s=0;
+        for(int val:unique){
+            if(val>0){
+                s+=val;
+            }
+        }
+        return s;
+    }
+};
