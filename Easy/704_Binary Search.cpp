f6592b548faa363e6707aa95e792e021ca6e7ac8
Abu Bakar Istiak <abubakar119147@gmail.com>
2026-08-20 12:23:08 +0600

    renamed all files

--- Easy/704_Binary Search.cpp
+++ Easy/704_Binary Search.cpp
@@ -6,8 +6,6 @@
  * * * * File    : _704_Binary Search.cpp
 */
 
-
-
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
@@ -23,10 +21,7 @@
 #define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 using namespace __gnu_pbds;
 using namespace std;
-
 template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
-
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
